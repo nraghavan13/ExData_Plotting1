eb0b133059ef2e417ddf09a0a71ade149a004fd3
Niyati Raghavan <niyati.raghavan@gmail.com>
2020-05-16 19:28:16 -0700

    Update plot1 script; adding plot2 src code and plot

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 #Read input data
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileURL,destfile="./dataset.zip")
@@ -12,14 +14,16 @@ plotData <- read.table(dataFile, header = T, sep=";", na.strings="?")
 plotData$Date <- as.Date(plotData$Date, format = "%d/%m/%Y")
 
 #Gets subset of data for dates 2007-02-01 and 2007-02-02
+#Set the date time for the plot generation
 subset <- subset(plotData, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+setDateTime <- strptime(paste(subset$Date, subset$Time), "%Y-%m-%d %H:%M:%S")
 
 #Gets "Global_active_power" of data from subset
 globalActivePowerData <- as.numeric(subset$Global_active_power)
 
 #PNG file with a width of 480 pixels and a height of 480 pixels
-png("plot1.png", width=480, height=480)
+png("plot2.png", width=480, height=480)
 
 #Construct plot
-hist(globalActivePowerData, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+plot(setDateTime, globalActivePowerData, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
